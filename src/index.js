d6119cf53d09e0fb96c0ff7ff9cf17f4267d0c5a
Pavel Khareuka <paul.horevko@gmail.com>
2019-03-17 12:51:21 +0300

    fix: task

--- src/index.js
+++ src/index.js
@@ -1,42 +1,28 @@
 module.exports = function getZerosCount(number, base) {
   
-  function findMultipliers (base) {
-    function isInt(n) {
-        return n % 1 === 0;
+  let numberSys = base;
+  let degree = 0;
+  let zeros = 0;
+  let primeNumber = 2;
+
+  while (true) {
+    if(numberSys % primeNumber === 0) {
+      numberSys /= primeNumber;
+      degree++;
+    } else {
+      primeNumber++;
+      degree = 0;
     }
 
-    let numberSys = base;
-    let divider = 0;
-    let i = 2;
-    while(i <= numberSys) {
-      if (isInt(numberSys / i) ) {
-        divider = i;
-          if ((numberSys / i) == 1) return divider;
-          numberSys /= i;
-          i = 2;
-      } else {
-        i++;
-      }
-    }
+    if (numberSys === 1) break;
   }
 
-  function findZero(number, devider) {
-    let dev = devider;
-    let result = 0;
-    let i = 1;
-    
-    while (true) {
-      if (number >= Math.pow(devider, i)) {
-        dev = Math.pow(devider, i);
-        result += Math.floor( number / dev );
-        i++;
-      } else {
-        return result;
-      }
-    }
+  while (number > 0) {
+      number = Math.floor( number / primeNumber);
+      zeros += number;
   }
-  
-  let deviderBase = findMultipliers(base);
-  findZero(number, deviderBase);
-  return findZero(number, deviderBase);
+
+  zeros = Math.floor(zeros/degree);
+
+  return zeros;
 }
